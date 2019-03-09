ca24af715e23fc7b9c29d87a849b1217ae1192a1
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-03-09 16:57:06 +0100

    Dollar-side effects managed, fixes #4 (p.23)

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -8,7 +8,7 @@ public class Dollar {
 
     public int amount;
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
