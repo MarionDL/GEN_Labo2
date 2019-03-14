d28dce7983dc239354ec4485e7518a70a79be544
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-03-14 16:52:31 +0100

    Addition test written, new classes Bank and Expression (p.54)

--- src/main/Money.java
+++ src/main/Money.java
@@ -1,6 +1,6 @@
 package main;
 
-public class Money {
+public class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -20,6 +20,10 @@ public class Money {
 
     Money times(int multiplier) {
         return new Money(amount * multiplier, currency);
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 
     static Money dollar(int amount) {
